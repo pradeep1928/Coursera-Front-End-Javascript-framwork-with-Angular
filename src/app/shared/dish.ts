b6881e2a2335b00561064a845252beb5b19e5f79
Pradeep padmukhi <ppadmukhi28@gmail.com>
2021-05-06 18:00:19 +0530

    Data binding of components

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { comment } from './comment';
+
 export class Dish {
     id: string;
     name: string;
@@ -7,4 +9,5 @@ export class Dish {
     label: string;
     price: string;
     description: string; 
+    comments: comment[];
 }
