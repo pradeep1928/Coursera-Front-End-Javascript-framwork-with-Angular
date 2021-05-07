573ffb8c59f9f0e1754ae2d144157c469e72b861
Pradeep padmukhi <ppadmukhi28@gmail.com>
2021-05-07 15:48:33 +0530

    basics on Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 
 
 @NgModule({
@@ -31,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
 
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
