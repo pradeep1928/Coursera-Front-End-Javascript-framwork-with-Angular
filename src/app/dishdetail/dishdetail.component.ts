8bddd2a31556ee347b705b60330a312d539eb9b3
Pradeep padmukhi <ppadmukhi28@gmail.com>
2021-05-26 16:02:46 +0530

    Rxjs Observable Part-1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -23,7 +23,7 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
     this.dishService.getDish(id)
-      .then(dish => this.dish = dish);
+      .subscribe(dish => this.dish = dish);
   }
 
   goBack(): void {
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +13,18 @@ export class LeaderService {
   constructor() { }
 
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve => {
-      //Simulate server latency with 2 seconds delay
-      setTimeout(() => resolve(LEADERS), 2000);
-    });
+  getLeaders(): Observable<Leader[]> {
+    return of(LEADERS).pipe(delay(2000));
   }
 
-  getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeader(id: string): Observable<Leader> {
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-    });
+  getFeaturedLeader(): Observable<Leader> {
+    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+  
   }
 }
 
