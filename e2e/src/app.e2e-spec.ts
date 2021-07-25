efdd04e7389754887a467b8908d494117d529441
Pradeep padmukhi <ppadmukhi28@gmail.com>
2021-07-25 16:09:36 +0530

    Angualr e2e testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,18 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to conFusion!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+  
 });
